c4380c89ddebc394a076a998ee6d6b42b08463d8
Marc Rozendaal <rozendaalmarc@gmail.com>
2023-07-09 02:28:28 +0200

    added basic game flow

--- GMTK_Jam/Assets/01_Scripts/GameManager.cs
+++ GMTK_Jam/Assets/01_Scripts/GameManager.cs
@@ -4,12 +4,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+
+    private GameState state;
+    public GameState gameState {
+        get {
+            return state;
+        }
+        set {
+            state = value;
+            HandleStateChanges();
+        }
+    }
+
+    [SerializeField]
+    private GameObject mainMenu;
+    [SerializeField]
+    private GameObject level;
+    [SerializeField]
+    private PlacementManager placementManager;
     
     private void Start() {
-        
+        gameState = GameState.MainMenu;
     }
 
     private void Update() {
+        
+    }
+
+    public void StartGame() {
+        gameState = GameState.Placing;
+    }
+
+    public void StartSimulating() {
+        gameState = GameState.Simulating;
+    }
+
+    private void HandleStateChanges() {
+
+        switch(gameState) {
+            
+            case GameState.MainMenu:
+                mainMenu.SetActive(true);
+                level.SetActive(false);
+                break;
+
+            case GameState.Placing:
+                mainMenu.SetActive(false);
+                level.SetActive(true);
+                placementManager.gameObject.SetActive(true);
+                break;
+
+            case GameState.Simulating:
+                mainMenu.SetActive(false);
+                level.SetActive(true);
+                placementManager.gameObject.SetActive(false);
+                break;
+
+        }
         
     }
     
